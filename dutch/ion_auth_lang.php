531b58c53a999051c9ad0af7b9ee6b22951ec7b9
Koen Rijpstra <k.rijpstra@gmail.com>
2014-03-02 12:01:48 +0100

    Added translation provided by Peter Ruiter - peterruiter.com

---
+++ dutch/ion_auth_lang.php
@@ -0,0 +1,48 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  Ion Auth Lang - Dutch
+*
+* Author: Ben Edmunds
+* 		  ben.edmunds@gmail.com
+*         @benedmunds
+* 
+* Location: http://github.com/benedmunds/ion_auth/
+* 
+* Translated by: Peter Ruiter, connectedconcepts.nl, based on first translation of Yvonne Emmerig, Dymago.nl
+* 
+* Created:  	 14.12.2012 
+* 
+* Description:  Dutch language file for Ion Auth messages and errors - Nederlandse vertaling voor Ion Auth berichten en foutmeldingen
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Account aanmaken is gelukt';
+$lang['account_creation_unsuccessful'] 	 	 = 'Niet mogelijk een account aan te maken';
+$lang['account_creation_duplicate_email'] 	 = 'Email is al in gebruik of ongeldig';
+$lang['account_creation_duplicate_username'] 	 = 'Gebruikersnaam is al in gebruik of ongeldig';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Wachtwoord is aangepast';
+$lang['password_change_unsuccessful'] 	  	 = 'Niet mogelijk wachtwoord aan te passen';
+$lang['forgot_password_successful'] 	 	 = 'Wachtwoord is gereset, email is verzonden';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Niet mogelijk wachtwoord te resetten';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Account is geactiveerd';
+$lang['activate_unsuccessful'] 		 	 = 'Niet mogelijk account te activeren';
+$lang['deactivate_successful'] 		  	 = 'Account is gedeactiveerd';
+$lang['deactivate_unsuccessful'] 	  	 = 'Niet mogelijk account te deactiveren';
+$lang['activation_email_successful'] 	  	 = 'Activatie email is verzonden';
+$lang['activation_email_unsuccessful']   	 = 'Niet mogelijk activatie email te versturen';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Met succes ingelogd';
+$lang['login_unsuccessful'] 		  	 = 'Onjuiste Login';
+$lang['logout_successful'] 		 	 = 'Met succes uitgelogd';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Account informatie is bijgewerkt';
+$lang['update_unsuccessful'] 		 	 = 'Kan accountinformatie niet bijwerken';
+$lang['delete_successful'] 		 	 = 'Gebruiker is verwijderd';
+$lang['delete_unsuccessful'] 		 	 = 'Kan gebruiker niet verwijderen';
